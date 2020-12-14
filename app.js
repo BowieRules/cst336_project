6a5d9ad4b075ea22d8891c6b05bbd735bf2ce5c0
Marianna Petrovich <Marianna.Petrovich@npd.com>
2020-12-13 20:51:39 -0800

    Another sync

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 const fetch = require('node-fetch');
 const express = require("express");
+const multer = require('multer');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
-
-const validator = require('./public/js/validator.js');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+const multers3 = require('multer-s3');
+const aws = require('aws-sdk');
 
 const app = express();
+
+// Create S3 service object
+const s3 = new aws.S3({
+    accessKeyId: `${process.env.AWS_ACCESS_KEY_ID}`,
+    secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`,
+    apiVersion: '2006-03-01',
+    region: 'us-west-1'
+});
+
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
-app.set('view engine', 'ejs');
+
+const validator = require('./public/js/validator.js');
+const pool = require('./dbPool.js');
+
+const uploadS3 = multer({
+  storage: multers3({
+    s3: s3,
+    acl: 'public-read',
+    bucket: 'csumb-marianna',
+    metadata: (req, file, cb) => {
+      cb(null, {fieldName: file.fieldname})
+    },
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString() + '-' + file.originalname)
+    }
+  })
+});
 
 app.use(session({
   secret: 'aloe vera',
@@ -17,10 +48,6 @@ app.use(session({
   saveUninitialized: true,
   cookie: { maxAge: 20 * 60 * 1000 } // cookie life in milliseconds = 20 minutes here
 }));
-
-app.get("/signIn", function(req, res){
-    res.render("signIn.ejs"); 
-});
 
 // DECLARING CUSTOM MIDDLEWARE
 const ifNotLoggedin = (req, res, next) => {
@@ -38,27 +65,218 @@ const ifLoggedin = (req,res,next) => {
 }
 // END OF CUSTOM MIDDLEWARE
 
-// routes
+//routes
+//root
+app.get("/", async (req, res) => {
+	res.render("home",{"isLoggedIn":req.session.logged_in}); //render
+}); //root
 
-// ROOT PAGE
-app.get("/", (req,res,next) => {
-    console.log(req.session.user);
-    res.render("index.ejs");
-});// END OF ROOT PAGE
-
-// Require login for my plants page
-app.get("/myplants", ifNotLoggedin, (req,res,next) => {
-    console.log(req.session.logged_in);
-    console.log(req.session.name);
-    console.log(req.session.login_id);
-    connection.query("SELECT `FirstName` FROM `Login` WHERE `LoginId`= ?",
-    [req.session.login_id], ( err, rows ) => {
+app.get("/api/insertPlant", function(req, res) {
+  let sql = "INSERT IGNORE INTO `Plant` (TrefleId, Common_Name, Scientific_Name, Family, Genus, Image_Url) VALUES (?,?,?,?,?,?)";
+  let sqlParams = [req.query.id,
+    decodeURIComponent(req.query.common_name),
+    decodeURIComponent(req.query.scientific_name),
+    decodeURIComponent(req.query.family),
+    decodeURIComponent(req.query.genus),
+    decodeURIComponent(req.query.image_url)];
+  pool.query(sql, sqlParams, function (err, rows, fields) {
     if (err) throw err;
-    // do something with the results here
-    res.render("myplants.ejs",{  name: rows[0].FirstName  });
-    });
-});// END OF ROOT PAGE
+    if (rows.affectedRows == 0)
+    {
+      sql = "SELECT PlantId FROM `Plant` WHERE TrefleId = ?";
+      pool.query(sql, [req.query.id], function (err, rows2, fields) {
+        if (err) throw err;
+        res.send(JSON.stringify(rows2));
+      });
+    }
+    else
+    {
+      res.send(JSON.stringify([{PlantId: rows.insertId}]));
+    }
+    
+  });
+});
 
+app.get("/api/insertLoginPlant", function(req, res) {
+  if(req.session.logged_in){
+    // check if plant is already in collection
+    pool.query("SELECT * FROM LoginPlant WHERE LoginId=? AND PlantId=?", [req.session.login_id, decodeURIComponent(req.query.id)], function (error, result) {
+      if (error) throw error;
+      if (result.length > 0) { res.send('This plant is already in your collection!'); }
+      else {
+        let sql = "INSERT INTO `LoginPlant` (LoginId, PlantId) VALUES (?,?)";
+        let sqlParams = [req.session.login_id,req.query.id];
+        pool.query(sql, sqlParams, function (err, rows, fields) {
+          if (err) throw err;
+          res.send('Your collection has been updated!');
+        });
+      }
+    });
+  }
+});
+
+//yourPlants : requires login
+app.get("/yourPlants", ifNotLoggedin, (req,res,next) => {
+
+  let sql = `SELECT Description, Hardiness, WaterFrequency, Soil, 
+  Temperature, LightExposure, Fertilization, FirstName, Common_Name,
+  Scientific_Name, Family, Genus, Image_Url, LoginPlant.PlantId, LoginPlant.LoginId
+  FROM Plant
+	  INNER JOIN LoginPlant
+      ON Plant.PlantId = LoginPlant.PlantId
+    INNER JOIN Login
+	    ON LoginPlant.LoginId = Login.LoginId
+  WHERE Login.LoginId = ?`;
+  
+    pool.query(sql, [req.session.login_id], (err, rows ) => {
+      if (err) throw err;
+      res.render("yourPlants", {"isLoggedIn":req.session.logged_in, yourPlantsArray: rows}); //render
+    }); //query
+}); //yourPlants
+
+//updatePlantProperties
+app.post("/updatePlantProperties", (req,res) => {
+
+  let sql = `UPDATE LoginPlant
+  SET
+	  Description = ?,
+    Hardiness = ?,
+    WaterFrequency = ?,
+    Soil = ?,
+    Temperature = ?,
+    LightExposure = ?,
+    Fertilization = ?
+  WHERE
+	  LoginId = ? AND PlantId = ?`;
+	  
+	//input validation; allow blank input
+	if(!(validator.lengthValid(req.body.description, 0, 500) || req.body.description.length == 0)) {
+	  res.send({message:"Error: Notes are not within 0 to 500 characters."});
+	}
+	else if(!(validator.lengthValid(req.body.hardiness, 0, 50) || req.body.hardiness.length == 0)) {
+	  res.send({message:"Error: Hardiness is not within 0 to 50 characters."});
+	}
+	else if(!(validator.lengthValid(req.body.waterFreq, 0, 50)  || req.body.waterFreq.length == 0)) {
+	  res.send({message:"Error: Water Frequency is not within 0 to 50 characters."});
+	}
+	else if(!(validator.lengthValid(req.body.soil, 0, 50) || req.body.soil.length == 0)) {
+	  res.send({message:"Error: Soil is not within 0 to 50 characters."});
+	}
+	else if(!(validator.lengthValid(req.body.temperature, 0, 50) || req.body.temperature.length == 0)) {
+	  res.send({message:"Error: Temperature is not within 0 to 50 characters."});
+	}
+	else if(!(validator.lengthValid(req.body.lightExposure, 0, 50) || req.body.lightExposure.length == 0)) {
+	  res.send({message:"Error: Light Exposure is not within 0 to 50 characters."});
+	}
+	else if(!(validator.lengthValid(req.body.fertilization, 0, 50) || req.body.fertilization.length == 0)) {
+	  res.send({message:"Error: Fertilization is not within 0 to 50 characters."});
+	}
+	else {
+  	let sqlParams = [req.body.description ,req.body.hardiness, req.body.waterFreq
+  	,req.body.soil, req.body.temperature, req.body.lightExposure, req.body.fertilization
+  	,req.body.LoginId, req.body.PlantId];
+  
+    pool.query(sql, sqlParams, (err, rows) => {
+      if(err) throw err;
+      console.log(rows.affectedRows.toString());
+      res.send({message:"Your plant data was saved!"});
+    }); //query
+	} //if else
+}); //updatePlantProperties
+
+//plantTalk
+app.get("/plantTalk", ifNotLoggedin, async (req, res) => {
+	res.render("plantTalk",{
+	  "isLoggedIn":req.session.logged_in
+	}); //render
+}); //plantTalk
+
+//plantTalk post
+app.post("/plantTalk", uploadS3.single('file1'), function (req, res, next) {
+  const file = req.file;
+  let topic = req.body.topic;
+  let posttext = req.body.posttext;
+  let plantid = req.body.usersPlant;
+  if (!file) {
+    //const error = new Error('Please upload a file');
+    //error.httpStatusCode = 400;
+    //return next(error);
+    res.render('plantTalk', {message: `Please upload a file`});
+  }
+  else if (plantid== "Select a plant to post about") {
+    //const error = new Error('Please select a plant');
+    //error.httpStatusCode = 400;
+    //return next(error);
+    res.render('plantTalk', {message: `Please select a plant`});
+  }
+  else if (topic.length == 0) {
+    //const error = new Error('Please enter a topic');
+    //error.httpStatusCode = 400;
+    //return next(error);
+    res.render('plantTalk', {message: `Please enter a topic`});
+  }
+  else if (posttext.length == 0) {
+    //const error = new Error('Please enter a post');
+    //error.httpStatusCode = 400;
+    //return next(error);
+    res.render('plantTalk', {message: `Please enter a post`});
+  }
+  else{
+    let filepath = req.file.location;
+    pool.query("INSERT INTO Post (PlantId, PostDte, Topic, PostText, Image_Url, LoginId) VALUES (?, CURRENT_TIMESTAMP(), ?, ?, ?, ?)",
+                  [plantid, topic, posttext, filepath, req.session.login_id], function (err, rows, fields) {
+          if (err) throw err;
+          res.render('plantTalk', {message: `You post was published successfully.`});
+    });
+  }
+}); //plantTalk post
+
+app.get("/api/getMyPlants",  function(req, res) {
+  let sql = "SELECT * FROM LoginPlant lp INNER JOIN Plant p on p.PlantId = lp.PlantId Where lp.LoginId = ? ORDER BY SortOrder";
+  pool.query(sql, [req.session.login_id], function (err, rows) {
+     if (err) throw err;
+     res.send(rows);
+  });  
+});//getMyPlants
+
+// get Plant Talk posts
+app.get("/api/getPosts",  function(req, res) {
+  let sql = "SELECT p.PostId, p.PlantId, p.PostDte, p.Topic, p.PostText, p.Image_Url, l.LoginName, pl.Common_Name, pl.Scientific_Name, pl.Family, pl.Genus, lp.Hardiness, lp.WaterFrequency, lp.Soil, lp.LightExposure, lp.Fertilization, lp.Description FROM Post p INNER JOIN Login l on p.LoginId = l.LoginId INNER JOIN LoginPlant lp on p.LoginId = lp.LoginId AND p.PlantId = lp.PlantId  INNER JOIN Plant pl on p.PlantId = pl.PlantId ORDER BY PostDte DESC";
+  pool.query(sql, function (err, rows) {
+     if (err) console.log(err);
+     res.send(rows);
+  });  
+});
+
+// post Plant Talk comments
+app.post("/addCommment",  function(req, res) {
+  let postId = req.body.postId;
+  let comment = req.body.userComments;
+  if(comment.length == 0){
+    res.render('plantTalk', {message: `Please add a comment`});
+  }
+  else{
+    pool.query("INSERT INTO Comment (PostId, CommentDte, LoginId, CommentText) VALUES (?, CURRENT_TIMESTAMP(), ?, ?)",
+                  [postId, req.session.login_id, comment], function (err, rows, fields) {
+          if (err) throw err;
+          res.render('plantTalk', {message: `You comment was published successfully.`});
+        });
+  }
+});
+
+// get Plant Talk comments
+app.get("/api/getComments",  function(req, res) {
+  let sql = "SELECT PostId, CommentDte, LoginName, CommentText FROM Comment c INNER JOIN Login l on c.LoginId = l.LoginId ORDER BY CommentDte DESC";
+  pool.query(sql, function (err, rows) {
+     if (err) console.log(err);
+     res.send(rows);
+  });  
+});
+
+//signIn
+app.get("/signIn", async (req, res) => {
+	res.render("signIn",{"isLoggedIn":req.session.logged_in}); //render
+}); //signIn
 
 app.post("/signIn", function(req, res) {
     
@@ -68,7 +286,7 @@ app.post("/signIn", function(req, res) {
   if (!validator.lengthValid(login, 5, 200) || !validator.lengthValid(password, 8, 20)) {
     res.render('signIn', {error: 'login or pass invalid'});
   } else {
-    connection.query("SELECT * FROM `Login` WHERE `LoginName` = ?", login, (error, result) => {
+    pool.query("SELECT * FROM `Login` WHERE `LoginName` = ?", login, (error, result) => {
       if (error) throw error;
       if (result.length === 0) {
         res.render('signIn', {error: 'No such user'});
@@ -89,10 +307,11 @@ app.post("/signIn", function(req, res) {
     });
   }
 });
-//show the register form
-app.get("/register", function(req, res) {
-    res.render("register.ejs");
-});
+
+//register
+app.get("/register", async (req, res) => {
+	res.render("register"); //render
+}); //register
 
 app.post('/register', function(req, res) {
   let email = req.body.email;
@@ -119,43 +338,62 @@ app.post('/register', function(req, res) {
     res.render('register', {error: 'Passwords do not match'});
   } else {
         // Check if email exists
-        connection.query("SELECT * FROM Login WHERE Email=?", [email], function (error, result) {
+        pool.query("SELECT * FROM Login WHERE Email=?", [email], function (error, result) {
+        if (error) throw error;
+        if (result.length > 0) { res.render('register', {error: 'This email already exists'}); }
+        else {
+          bcrypt.genSalt(10, function(error, salt) {
             if (error) throw error;
-            if (result.length > 0) { res.render('register', {error: 'This email already exists'}); }
-            else {
-              bcrypt.genSalt(10, function(error, salt) {
-                if (error) throw error;
-                bcrypt.hash(password, salt, function(error, hash) {
-                  if (error) throw error;
-                  connection.query("INSERT INTO Login (LoginName, HashedPwd, Email, FirstName, LastName, Gender, ZipCode) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                  [login, hash, email, fname, lname, gender, zip], function(error, result) {
-                  res.render('register', {message: `You have been registered, try logging in`});
-                  });
-                });
+            bcrypt.hash(password, salt, function(error, hash) {
+              if (error) throw error;
+              pool.query("INSERT INTO Login (LoginName, HashedPwd, Email, FirstName, LastName, Gender, ZipCode) VALUES (?, ?, ?, ?, ?, ?, ?)",
+              [login, hash, email, fname, lname, gender, zip], function(error, result) {
+                res.render('register', {message: `You have been registered, try logging in`});
               });
-            } });
+            });
+          });
+        } });
   }
 });
 
-// app cant access browser because it runs on server side,
-app.get("/api/trefle", async function(req, res){
-    let apiUrl = "https://trefle.io/api/v1/plants?token=6t4ZVV4DE7bKaqSg1CDFPHq3r5giNXINF3qlk43Povk";
-    let response = await fetch(apiUrl);
-    let data = await response.json();
-    res.send(data);
+//findPlants
+app.get("/findPlants", async (req, res) => {
+	res.render("findPlants",{"isLoggedIn":req.session.logged_in}); //render
 });
 
-// database
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'qc0w3lq0xdqs5ny3',
-    password: 'ea3ex48nw4hd4v3q',
-    database: 'sn1qvahom0zodcij'
-});
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
+//logout
+app.get("/signout", (req,res) => {
+  req.session.destroy();
+  res.redirect("/");
+}); //logout
+
+//search for plants
+app.get("/search", async (req, res) => {
+    let keyword = "";
+    if (req.query.keyword){
+        keyword = req.query.keyword;
+        let apiUrl = `https://trefle.io/api/v1/plants/search?token=6t4ZVV4DE7bKaqSg1CDFPHq3r5giNXINF3qlk43Povk&q=${keyword}`;
+        let response = await fetch(apiUrl);
+        let data = await response.json();
+        data = data.data;
+        let idArray = [];
+        let imageUrlArray = [];
+        let commonNameArray = [];
+        let scienceNameArray = [];
+        let genusArray = [];
+        let familyNameArray = [];
+        for(let i = 0; i < data.length; i++)
+        {
+            idArray.push(data[i].id);
+            imageUrlArray.push(data[i].image_url);
+            commonNameArray.push(data[i].common_name);
+            scienceNameArray.push(data[i].scientific_name);
+            familyNameArray.push(data[i].family);
+            genusArray.push(data[i].genus);
+        }
+        let isVisible = (req.session.logged_in)? "": "hidden";
+        res.render("results", {"isLoggedIn":req.session.logged_in, "idArray":idArray, "imageUrlArray":imageUrlArray, "commonNameArray":commonNameArray, "scienceNameArray":scienceNameArray,"familyNameArray": familyNameArray, "genusArray":genusArray, "isVisible": isVisible});
+    }
 });
 
 // starting server
